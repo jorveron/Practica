4856777dd3a1ede622338c923dedce14bb268cc9
Jorge Veron <jorgeveron93@gmail.com>
2023-06-10 13:02:59 -0400

    programación de los botones seleccionar mascotas y seleccionar ataque

--- monsterswar/js/monsterswar.js
+++ monsterswar/js/monsterswar.js
@@ -1,7 +1,23 @@
-function seleccionarMascotaJugador() {
-    alert("SELECCIONASTE TU MASCOTA")
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota')
+function seleccionarMascotaJugador() {
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    
+    if (inputHipodoge.checked) {
+        alert("Tu mascota es Hipodoge. A luchar!")
+    } else if (inputCapipepo.checked) {
+        alert("Tu mascota es Capipepo. Es hora del duelo!")
+    } else if (inputRatigueya.checked) {
+        alert("Tu mascota es Ratigueya. Que comience la diversión!")
+    } else {
+        alert("Debes seleccionar una mascota para continuar.")
+    }
+    
+}
 
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+window.addEventListener('load', iniciarJuego)
